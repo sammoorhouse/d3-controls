4e260ef531066ddec971c7f657515d20b5877e3b
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2015-10-07 23:17:51 -0400

    settings

--- CircularProgress.js
+++ CircularProgress.js
@@ -1,70 +1,59 @@
 function CircularProgress(element, settings){
-	this.element = element;
-	this.settings = settings;
+	var duration = settings.duration || 500;
+	var w = settings.width || 200;
+	var h = settings.height || w;
+	var outerRadius = settings.outerRadius || w/2;
+	var innerRadius = settings.innerRadius || (w/2) * (80/100);
+	var range = settings.range || {min: 0, max: 100};
+	var fill = settings.fill || "#F20100";
 	
-	this.duration = settings.duration || 500;
-					
-	this.w = settings.width || 200;
-	this.h = settings.height || this.w;
-		
-	this.outerRadius = settings.outerRadius || this.w/2;
-	this.innerRadius = settings.innerRadius || (this.w/2) * (80/100);
-	
-	this.range = settings.range || {min: 0, max: 100};
-	
-	this.fill = settings.fill || "#F20100";
-	
-	this.svg = d3.select("body")
+	var svg = d3.select(element)
 		.append("svg")
-			.attr("width", this.w)
-			.attr("height", this.h);
+			.attr("width", w)
+			.attr("height", h);
 	
-	CircularProgress.prototype.arc = 
-		d3.svg.arc()
-			.innerRadius(this.innerRadius)
-			.outerRadius(this.outerRadius);
+	var arc = d3.svg.arc()
+			.innerRadius(innerRadius)
+			.outerRadius(outerRadius);
 	
-	CircularProgress.prototype.paths = function(numerators) {
-		var range = this.range;
+	var paths = function(numerators) {
 		return numerators.map(function(numerator){
-			var degrees = (numerator/range.max) * 360.0;
+			var degrees = ((numerator - range.min) / (range.max - range.min)) * 360.0;
 			var radians = degrees * (Math.PI / 180);
 			var data = {value: numerator, startAngle: 0, endAngle: radians};
 			return data;
 		});
 	}
 	
-	this.g = this.svg.append('g')
-		.attr('transform', 'translate(' + this.w / 2 + ',' + this.h / 2 + ')');
+	var g = svg.append('g').attr('transform', 'translate(' + w / 2 + ',' + h / 2 + ')');
 	
 	//initialise the control
-	this.g.datum([0]).selectAll("path")
-		.data(CircularProgress.prototype.paths)
+	g.datum([0]).selectAll("path")
+		.data(paths)
 	.enter()
 		.append("path")
-		.attr("fill", this.fill)
-		.attr("d", CircularProgress.prototype.arc)
+		.attr("fill", fill)
+		.attr("d", arc)
 	.each(function(d){ this._current = d; });
 	
-	this.svg.datum([0]).selectAll("text")
-		.data(CircularProgress.prototype.paths)
+	svg.datum([0]).selectAll("text")
+		.data(paths)
 	.enter()
 		.append("text")
-		.attr("transform", "translate(" + this.w/2 + ", " + this.h/1.6 + ")")
+		.attr("transform", "translate(" + w/2 + ", " + h/1.6 + ")")
 		.attr("text-anchor", "middle")
 		.text(function(d){return d.value});
 	
-};
-
-CircularProgress.prototype.update = function(percent) {
-	this.g.datum(percent).selectAll("path").data(CircularProgress.prototype.paths).transition().duration(this.duration).attrTween("d", CircularProgress.prototype.arcTween);
-	this.svg.datum(percent).selectAll("text").data(CircularProgress.prototype.paths).text(function(d){return d.value;});
-};
-
-CircularProgress.prototype.arcTween = function(a) {
-	var i = d3.interpolate(this._current, a);
-	this._current = i(0);
-	return function(t) {
-		return CircularProgress.prototype.arc(i(t));
+	this.update = function(percent) {
+		g.datum(percent).selectAll("path").data(paths).transition().duration(duration).attrTween("d", arcTween);
+		svg.datum(percent).selectAll("text").data(paths).text(function(d){return d.value;});
 	};
-}
+	
+	var arcTween = function(initial) {
+		var interpolate = d3.interpolate(this._current, initial);
+		this._current = interpolate(0);
+		return function(t) {
+			return arc(interpolate(t));
+		};
+	}
+};
